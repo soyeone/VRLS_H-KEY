d1365f24ce9e3a4b7cd0053086d796730b6358fb
jinwalya <32252182+jinwalya@users.noreply.github.com>
2018-05-25 15:06:31 +0900

    5.25

--- VRLS_UI_Instrument/Assets/Scripts/selectplaybutton.cs
+++ VRLS_UI_Instrument/Assets/Scripts/selectplaybutton.cs
@@ -5,8 +5,6 @@ using UnityEngine.UI;
 
 public class selectplaybutton : MonoBehaviour
 {
-
-	private bool isplaybutton = false;
 	public GameObject selectplaybtn;
 	private AudioSource[] audiosources = new AudioSource[100]; // 임의로 100개만;
 	// Use this for initialization
@@ -23,49 +21,38 @@ public class selectplaybutton : MonoBehaviour
 
 	public void selectplay()
 	{
-        int k = 0;
+		int k = 0;
 
-        GameObject[] obj;
-		obj = GameObject.FindGameObjectsWithTag("select");
+		GameObject[] obj;
+		obj = GameObject.FindGameObjectsWithTag ("select");
 
-        int i = 0;
+		int i = 0;
 
-        foreach (GameObject select in obj)
-        {
-            audiosources[i] = obj[i].GetComponent<AudioSource>();
-            audiosources[i].Stop();
-            k++;
-            i++;
-        }
-
-        if (isplaybutton)
-		{
-			//GameObject obj = GameObject.FindGameObjectWithTag("select");
-			//AudioSource source;
-			//source = obj.GetComponent<AudioSource>();
-			/*foreach (GameObject select in obj)
-            {
-				audiosources [l] = obj [l].GetComponent<AudioSource> ();
-				audiosources [l].Stop ();
-				k++;
-				l++;
-            }*/
-
-			for(int o = 0; o<k; o++){
-				audiosources [o].Stop ();
-			}
-			isplaybutton = false;
-			selectplaybtn.GetComponentInChildren<Text>().text = "play";
+		foreach (GameObject select in obj) {
+			audiosources [i] = obj [i].GetComponent<AudioSource> ();
+			audiosources [i].Stop ();
+			k++;
+			i++;
 		}
-		else
-		{
-			for(int j = 0; j<k; j++)
-			{
-				audiosources[j].Play();
-			}
-			isplaybutton = true;
-			selectplaybtn.GetComponentInChildren<Text>().text = "stop";
+
+		for (int o = 0; o < k; o++) {
+			audiosources [o].Play ();
 		}
 	}
 
+
+	public void selectstop()
+	{
+
+		GameObject[] obj;
+		obj = GameObject.FindGameObjectsWithTag ("select");
+
+		int i = 0;
+
+		foreach (GameObject select in obj) {
+			audiosources [i] = obj [i].GetComponent<AudioSource> ();
+			audiosources [i].Stop ();
+			i++;
+		}
+	}
 }
